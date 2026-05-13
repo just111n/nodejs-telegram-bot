4265ccdc58a005f0a498d14951de82352babcda6
Justin Looi <justinlooijw@gmail.com>
2026-05-13 20:32:28 +0800

    feat: add cache and pre-commit hooks

---
+++ src/config/cache.js
@@ -0,0 +1,21 @@
+const { createClient } = require("redis");
+
+const redisClient = createClient({
+  username: "default",
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+});
+
+redisClient.on("error", (err) => {
+  console.log("Redis Client Error", err);
+});
+
+(async () => {
+  await redisClient.connect();
+  console.log("Redis connected");
+})();
+
+module.exports = redisClient;
